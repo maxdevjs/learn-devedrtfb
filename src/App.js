0471a1b25445c90f6f3bbc3ecbbfdf3ed9d8eb0b
Massimiliano Giroldi <max.devjs@gmail.com>
2019-04-11 02:44:52 -0300

    Set dynamic state up

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import './App.css';
 
 function App() {
 
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([
+    {name: "Ed", message: "Hello there"},
+    {name: "John", message: "I am John Snow"},
+    {name: "traversy", message: "I am me"},
+  ]);
   
-  const increment = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-  };
 
   return (
     <div className="app">
-      <h1 className={isRed ? "red" : ""}>Hello React</h1>
-      <Tweet className={isRed ? "red" : ""} name="What" body="This is a random tweet" nol="32487623764623"/>
-      <Tweet className={isRed ? "red" : ""}  name="Why" body="This is NOT a random tweet" nol="0"/>
-      <button onClick={increment}>Increment</button>
-      <h1 className={isRed ? "red" : ""} >{count}</h1>
+      {users.map(user => (
+        <Tweet name={user.name} body={user.message} />
+      ))}
     </div>
   );
 }
