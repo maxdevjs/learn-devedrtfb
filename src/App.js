e21c8f379bdda844571e79929096abb88ee2ae1e
Massimiliano Giroldi <max.devjs@gmail.com>
2019-04-11 02:37:24 -0300

    Set isRed up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Hello from './sayHello';
+import React, {useState} from 'react';
 import Tweet from './tweet';
+
+import './App.css';
 
 function App() {
 
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+  
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  };
+
   return (
     <div className="app">
-      <h1>Hello React</h1>
-      <Hello />
-      <Tweet name="What" body="This is a random tweet" nol="32487623764623"/>
-      <Tweet name="Why" body="This is NOT a random tweet" nol="0"/>
+      <h1 className={isRed ? "red" : ""}>Hello React</h1>
+      <Tweet className={isRed ? "red" : ""} name="What" body="This is a random tweet" nol="32487623764623"/>
+      <Tweet className={isRed ? "red" : ""}  name="Why" body="This is NOT a random tweet" nol="0"/>
+      <button onClick={increment}>Increment</button>
+      <h1 className={isRed ? "red" : ""} >{count}</h1>
     </div>
   );
 }
